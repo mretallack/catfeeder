da006d8b93441e3402e3d13e0153d5116cf5fce4
Mark Retallack <mark.retallack@siemens.com>
2021-06-17 13:14:21 +0100

    Added more measurements

--- tools/battery.py
+++ tools/battery.py
@@ -7,8 +7,8 @@ import matplotlib.pyplot as plt
 # set the points here
 # use y for voltage
 # and x for raw value
-y=[5.254, 5.96]
-x=[164, 193]
+y=[5.254, 5.77, 5.85, 5.96, 5.66]
+x=[164, 187, 189, 193, 182]
 
 m=(y[1]-y[0])/(x[1]-x[0])
 
